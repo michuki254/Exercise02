95c87b78e9f750eed13970dfd1340f91e6125d1d
Boniface Michuki <dxmichuki@yahoo.com>
2021-08-13 15:13:21 +0300

    Add project files.

---
+++ Exercise02/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Exercise01;
+
+namespace Exercise02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter a number to convert to words: ");
+            ulong n = ulong.Parse(Console.ReadLine());
+
+            Console.WriteLine("{0}", Exercise01.Exercise01.TowardsWrapper(n)) ;
+        }
+    }
+}
